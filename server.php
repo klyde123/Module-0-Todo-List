7da74456f582079ee8ac782b20c066e81c65b631
Klyde Louise Olitoquit <kl.olitoquit@gmail.com>
2020-12-10 21:25:59 +0800

    adding redirecting the index page after inserting

--- server.php
+++ server.php
@@ -3,6 +3,7 @@
 	//adding variables
 	$name = "";
 	$discription = "";
+	$id = 0;
 
 	//connect to database
 	$db = mysqli_connect('localhost', 'root', '', 'crud');
@@ -11,6 +12,10 @@
 	if (isset($_POST['save'])){
 		$name = $_POST['name'];
 		$discription = $_POST['discription']; //discription
+
+		$query = "INSERT INTO info (name, discription) VALUES ('$name', '$discription')";
+		mysqli_query($db, $query);
+		header('location: index.php'); //this is for redirecting the index page after inserting
 	}
 
 
