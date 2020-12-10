617cef88729b3927325df0548afc9dbc8b4e152e
Klyde Louise Olitoquit <kl.olitoquit@gmail.com>
2020-12-10 10:22:30 +0800

    Adding table tag and create th and td

--- index.php
+++ index.php
@@ -6,5 +6,24 @@
 <body>
 <!--This is my first commit for this simple crud -->
 <!--my 2nd attempt for commit and push-->
+
+<table>
+	<thead>
+		<tr>
+			<th>Title</th>
+			<th>Description</th>
+			<th colspan="2"> Option</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>Module 0</td>
+			<td>Make a repositor at Github and make a simple project</td>
+			
+		</tr>
+	</tbody>
+</table>
+
+
 </body>
 </html>
