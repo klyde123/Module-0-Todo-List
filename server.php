0725f2fbb76e198b250e4b938dd410c6b718947f
Klyde Louise Olitoquit <kl.olitoquit@gmail.com>
2020-12-10 21:12:55 +0800

    adding function of connect to database and if click the button of save

---
+++ server.php
@@ -0,0 +1,13 @@
+<?php
+
+	//connect to database
+	$db = mysqli_connect('localhost', 'root', '', 'crud');
+
+	//if save button is clicked
+	if (isset($_POST['save'])){
+		$name = $_POST['name'];
+		$discription = $_POST['discription']; //discription
+	}
+
+
+?>
