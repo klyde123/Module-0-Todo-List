770c3d2c29830406f7ac489de4bb1d808dea9e99
Klyde Louise Olitoquit <kl.olitoquit@gmail.com>
2020-12-11 21:34:41 +0800

    displays a confirmation message to tell the user that a new record has been created

--- index.php
+++ index.php
@@ -10,6 +10,15 @@
 <!--This is my first commit for this simple crud -->
 <!--my 2nd attempt for commit and push-->
 
+<?php if (isset($_SESSION['message'])): ?>
+	<div class="msg">
+		<?php 
+			echo $_SESSION['message']; 
+			unset($_SESSION['message']);
+		?>
+	</div>
+<?php endif ?>
+
 <table>
 	<thead>
 		<tr>
