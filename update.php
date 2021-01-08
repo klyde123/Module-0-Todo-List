bce6a670d5ecef940a97ca3dfa8d0900effd45ca
Klyde Louise Olitoquit <kl.olitoquit@gmail.com>
2021-01-08 21:40:16 +0800

    edit the form because of some error and add a variable to execute the sql

--- update.php
+++ update.php
@@ -6,12 +6,10 @@ include "config.php";
 		$firstname = $_POST['firstname'];
 		$user_id = $_POST['user_id'];
 		$lastname = $_POST['lastname'];
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-		$gender = $_POST['gender'];
+	
 
 		// write the update query
-		$sql = "UPDATE `users` SET `firstname`='$firstname',`lastname`='$lastname',`email`='$email',`password`='$password',`gender`='$gender' WHERE `id`='$user_id'";
+		$sql = "UPDATE `users` SET `firstname`='$firstname',`lastname`='$lastname' WHERE `id`='$user_id'";
 
 		// execute the query
 		$result = $conn->query($sql);
@@ -22,40 +20,53 @@ include "config.php";
 			echo "Error:" . $sql . "<br>" . $conn->error;
 		}
 	}
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>
-		UPDATE
-	</title>
-</head>
-<body>
 
 
-<form action="" method="post">
+// if the 'id' variable is set in the URL, we know that we need to edit a record
+if (isset($_GET['id'])) {
+	$user_id = $_GET['id'];
+
+	// write SQL to get user data
+	$sql = "SELECT * FROM `users` WHERE `id`='$user_id'";
+
+	//Execute the sql
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		
+		while ($row = $result->fetch_assoc()) {
+			$first_name = $row['firstname'];
+			$lastname = $row['lastname'];
+			$id = $row['id'];
+		}
+
+	?>
+		<h2>User Update Form</h2>
+		<form action="" method="post">
 		  <fieldset>
 		    <legend>Personal information:</legend>
-		    First name:<br>
+		    Title:<br>
 		    <input type="text" name="firstname" value="<?php echo $first_name; ?>">
 		    <input type="hidden" name="user_id" value="<?php echo $id; ?>">
 		    <br>
-		    Last name:<br>
+		    Description:<br>
 		    <input type="text" name="lastname" value="<?php echo $lastname; ?>">
-		    <br>
-		    Email:<br>
-		    <input type="email" name="email" value="<?php echo $email; ?>">
-		    <br>
-		    Password:<br>
-		    <input type="password" name="password" value="<?php echo $password; ?>">
-		    <br>
-		    Gender:<br>
-		    <input type="radio" name="gender" value="Male" <?php if($gender == 'Male'){ echo "checked";} ?> >Male
-		    <input type="radio" name="gender" value="Female" <?php if($gender == 'Female'){ echo "checked";} ?>>Female
 		    <br><br>
 		    <input type="submit" value="Update" name="update">
 		  </fieldset>
 		</form>
-</body>
-</html>
+
+		</body>
+		</html>
+
+
+
+
+	<?php
+	} else{
+		// If the 'id' value is not valid, redirect the user back to view.php page
+		header('Location: view.php');
+	}
+
+}
+?>
