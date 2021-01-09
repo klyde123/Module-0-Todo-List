4820b4123210b30f094677156dd1c9adeece88f7
Klyde Louise Olitoquit <kl.olitoquit@gmail.com>
2021-01-09 12:25:54 +0800

    adding style in refresh button

--- index.php
+++ index.php
@@ -60,6 +60,41 @@ form {
     border-radius: 5px;
 }
 
+.button {
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.button1 {
+  background-color: white;
+  color: black;
+  border: 2px solid #4CAF50;
+}
+
+.button1:hover {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.button2 {
+  background-color: white;
+  color: black;
+  border: 2px solid #008CBA;
+}
+
+.button2:hover {
+  background-color: #008CBA;
+  color: white;
+}
+
 </style>
 <body>
 
@@ -80,7 +115,7 @@ form {
 
 <br><br>
 <center>
-		<button onclick="window.location.href='index.php'">REFRESH</button>
+		<button class="button button1" onclick="window.location.href='index.php'">REFRESH</button>
 		</center>
 
 
